14f3175c4b2e8c2f7b9b8ae3bcf269ad6b8d436a
blabla <tleve056@ottawa.ca>
2017-07-09 23:38:54 -0400

    ID 6 - added onClick Listeners to registration buttons, and added method to enable error messages for TextInputLayout

--- app/src/main/java/com/betterbet/betterbet/RegistrationActivity.java
+++ app/src/main/java/com/betterbet/betterbet/RegistrationActivity.java
@@ -1,11 +1,14 @@
 package com.betterbet.betterbet;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -17,6 +20,48 @@ public class RegistrationActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setTextInputLayoutErrorEnabled(false);
+        setCancelButtonListneners();
+        setCreateAccountButtonListeners();
+
+    }
+
+    private void setTextInputLayoutErrorEnabled(boolean enabled) {
+        TextInputLayout username = (TextInputLayout) findViewById(R.id.username);
+        username.setErrorEnabled(true);
+        TextInputLayout firstName = (TextInputLayout) findViewById(R.id.firstName);
+        firstName.setErrorEnabled(true);
+        TextInputLayout lastName = (TextInputLayout) findViewById(R.id.lastName);
+        lastName.setErrorEnabled(true);
+        TextInputLayout phoneNumber = (TextInputLayout) findViewById(R.id.phoneNumber);
+        phoneNumber.setErrorEnabled(true);
+        TextInputLayout email = (TextInputLayout) findViewById(R.id.email);
+        email.setErrorEnabled(true);
+        TextInputLayout password = (TextInputLayout) findViewById(R.id.password);
+        password.setErrorEnabled(true);
+        TextInputLayout confirmPassword = (TextInputLayout) findViewById(R.id.confirmPassword);
+        confirmPassword.setErrorEnabled(true);
+    }
+
+    private void setCancelButtonListneners() {
+        Button cancelBtn = (Button) findViewById(R.id.cancelBtn);
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    private void setCreateAccountButtonListeners() {
+        Button createAccountBtn = (Button) findViewById(R.id.createAccountBtn);
+        createAccountBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent MainIntent = new Intent(RegistrationActivity.this, MainActivity.class);
+                startActivity(MainIntent);
+            }
+        });
     }
 
 }
