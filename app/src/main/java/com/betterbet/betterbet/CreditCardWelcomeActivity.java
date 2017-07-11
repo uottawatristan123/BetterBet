8144d8a92378358a51c7d14e813763c749e47f31
blabla <tleve056@ottawa.ca>
2017-07-10 23:40:57 -0400

    ID 16 - Added  empty Credit Card Welcome Activity and populated the Navigation Drawer

---
+++ app/src/main/java/com/betterbet/betterbet/CreditCardWelcomeActivity.java
@@ -0,0 +1,13 @@
+package com.betterbet.betterbet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreditCardWelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credit_card_welcome);
+    }
+}
--- app/src/main/java/com/betterbet/betterbet/MainActivity.java
+++ app/src/main/java/com/betterbet/betterbet/MainActivity.java
@@ -80,17 +80,21 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_games) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_mybets) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_statistics) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_collect) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_myaccount) {
+
+        }else if (id == R.id.nav_contact) {
+
+        }else if (id == R.id.nav_logout) {
 
         }
 
