2463a19b117e0dea4498591524a6ba68176f9598
blabla <tleve056@ottawa.ca>
2017-07-11 00:48:32 -0400

    ID 17 - Initial layout for the credit card and billing information activity

--- app/src/main/java/com/betterbet/betterbet/CreditCardWelcomeActivity.java
+++ app/src/main/java/com/betterbet/betterbet/CreditCardWelcomeActivity.java
@@ -3,11 +3,31 @@ package com.betterbet.betterbet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.braintreepayments.cardform.utils.CardType;
+import com.braintreepayments.cardform.view.CardForm;
+import com.braintreepayments.cardform.view.SupportedCardTypesView;
+
 public class CreditCardWelcomeActivity extends AppCompatActivity {
+
+    private static final CardType[] SUPPORTED_CARD_TYPES = { CardType.VISA, CardType.MASTERCARD, CardType.AMEX };
+
+    private SupportedCardTypesView mSupportedCardTypesView;
+
+    protected CardForm mCardForm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credit_card_welcome);
+
+        mSupportedCardTypesView = (SupportedCardTypesView) findViewById(R.id.supported_card_types);
+        mSupportedCardTypesView.setSupportedCardTypes(SUPPORTED_CARD_TYPES);
+
+        CardForm cardForm = (CardForm) findViewById(R.id.card_form);
+        cardForm.cardRequired(true)
+                .expirationRequired(true)
+                .cvvRequired(true)
+                .actionLabel("Purchase")
+                .setup(this);
     }
 }
