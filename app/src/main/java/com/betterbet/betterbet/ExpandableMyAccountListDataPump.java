6ce19b937d6f0a8b259878969f49a10237dd0d95
blabla <tleve056@ottawa.ca>
2017-07-12 18:16:49 -0400

    ID 31 - Custom Expandable My Account List Adapter

--- app/src/main/java/com/betterbet/betterbet/ExpandableMyAccountListDataPump.java
+++ app/src/main/java/com/betterbet/betterbet/ExpandableMyAccountListDataPump.java
@@ -10,17 +10,17 @@ public class ExpandableMyAccountListDataPump {
 
         List<String> persInformation = new ArrayList<String>();
         persInformation.add("Username: abc@gmail.com");
-        persInformation.add("First Name: Conor");
-        persInformation.add("Last Name: McGregor");
+        persInformation.add("First Name: Jimmy");
+        persInformation.add("Last Name: Cricket");
         persInformation.add("Email: abc@gmail.com");
         persInformation.add("Phone Number: 613-888-0939");
 
         List<String> billingAddress = new ArrayList<String>();
-        billingAddress.add("United States");
-        billingAddress.add("Spain");
-        billingAddress.add("Argentina");
-        billingAddress.add("France");
-        billingAddress.add("Russia");
+        billingAddress.add("First Name: Jimmy");
+        billingAddress.add("Last Namw: Cricket");
+        billingAddress.add("Apt./Suite #: 2");
+        billingAddress.add("Address: 123 main street");
+        billingAddress.add("Postal Code: K5V 2G4");
 
         List<String> creditCard = new ArrayList<String>();
         creditCard.add("Master Card XXXX XXXX XXXX 2323");
